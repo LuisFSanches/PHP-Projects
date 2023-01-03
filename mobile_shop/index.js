1661d6bc76af52dd2c8ca0add8fbaa77abc9fba9
LuisF <luis-lipe@hotmail.com>
2023-01-03 12:59:39 -0300

    Product page finished

--- mobile_shop/index.js
+++ mobile_shop/index.js
@@ -61,4 +61,25 @@ $(document).ready(function() {
       },
     }
   });
+
+  // product qty section
+  let $qty_up = $(".qty .qty-up");
+  let $qty_down = $(".qty .qty-down");
+  let $input = $(".qty .qty_input");
+  //click on qty up button
+  $qty_up.click(function(e){
+    if ($input.val() >= 1 && $input.val() <= 9) {
+      $input.val(function(i, oldval) {
+        return ++oldval;
+      })
+    }
+  });
+
+  $qty_down.click(function(e){
+    if ($input.val() > 1 && $input.val() <= 10) {
+      $input.val(function(i, oldval) {
+        return --oldval;
+      })
+    }
+  });
 });
